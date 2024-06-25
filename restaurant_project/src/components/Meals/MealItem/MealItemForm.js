5c083569555efe4f599b3a0c34e1d3c0ec1d3311
abhi4450 <abhishek.anshu1991>
2024-06-25 15:32:51 +0530

    added MealItem form to to take amount as input for each meal item

--- restaurant_project/src/components/Meals/MealItem/MealItemForm.js
+++ restaurant_project/src/components/Meals/MealItem/MealItemForm.js
@@ -1,5 +1,11 @@
+import classes from "./MealItemForm.module.css"
 
-
-const MealItemForm = () => {};
+const MealItemForm = () => {
+    return <form>
+        <label htmlFor="amount" >Amount</label>
+        <input className={classes.input} type="number" id="amount" />
+        <button className={classes.button}>+ Add</button>
+    </form>
+};
 
 export default MealItemForm;
