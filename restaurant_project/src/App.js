cb24b9e20ab3ae3cbb0d015e48b66f214f108db4
abhi4450 <abhishek.anshu1991>
2024-06-25 14:31:36 +0530

    added restaurant summary in MealsSummary component

--- restaurant_project/src/App.js
+++ restaurant_project/src/App.js
@@ -1,11 +1,12 @@
-import React from "react";
+import React,{Fragment} from "react";
 import Header from "./components/Layout/Header";
-
+import Meals from "./components/Meals/Meals";
 function App() {
   return (
-    <>
+    <Fragment>
       <Header />
-    </>
+      <Meals />
+      </Fragment>
   );
 }
 
---
+++ restaurant_project/src/components/Meals/Meals.js
@@ -0,0 +1,11 @@
+import React from "react";
+import MealsSummary from "./MealsSummary";
+const Meals = () => {
+  return (
+    <React.Fragment>
+      <MealsSummary />
+    </React.Fragment>
+  );
+};
+
+export default Meals;
