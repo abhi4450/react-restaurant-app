1332751f6944ceebfa8e2059ee848f4ff1820e0a
abhi4450 <abhishek.anshu1991>
2024-06-25 13:58:14 +0530

    created header component with title and cart button

---
+++ restaurant_project/src/components/Layout/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+import classes from "./Header.module.css";
+import HeaderCartButton from "./HeaderCartButton";
+
+const Header = () => {
+    return (
+        <>
+            <header className={classes.header}>
+                <h1>Restraunt Meals</h1>
+               <HeaderCartButton />
+            </header>;
+            </>
+    )
+};
+
+export default Header;
