2e725e98deda37729633f03edd3fc9c2e36313f6
abhi4450 <abhishek.anshu1991>
2024-06-25 14:50:18 +0530

    added MealsSummary and displaying available meals items below the summary by mapping into a listitem

--- restaurant_project/src/components/Meals/Meals.js
+++ restaurant_project/src/components/Meals/Meals.js
@@ -1,9 +1,11 @@
 import React from "react";
 import MealsSummary from "./MealsSummary";
+import AvailableMeals from "./AvailableMeals";
 const Meals = () => {
   return (
     <React.Fragment>
       <MealsSummary />
+      <AvailableMeals />
     </React.Fragment>
   );
 };
