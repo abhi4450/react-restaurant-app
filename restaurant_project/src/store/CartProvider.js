003e4d2a8a7ca451d87d9843c48bdb19c9e7b81d
abhi4450 <abhishek.anshu1991>
2024-07-07 17:55:38 +0530

    added functionality to remove item from cart

--- restaurant_project/src/store/CartProvider.js
+++ restaurant_project/src/store/CartProvider.js
@@ -32,6 +32,26 @@ const cartReducer = (state, action) => {
   }
 
   if (action.type === "REMOVE") {
+    let updatedItems;
+
+    const existingItemIndex = state.items.findIndex(
+      (item) => item.id === action.id
+    );
+
+    const existingItem = state.items[existingItemIndex];
+    const updatedTotalAmount = state.totalAmount - existingItem.price;
+    if (existingItem.amount === 1) {
+      updatedItems = state.items.filter((item) => item.id !== action.id);
+    } else {
+      let updatedItem = { ...existingItem, amount: existingItem.amount - 1 };
+      updatedItems = [...state.items];
+      updatedItems[existingItemIndex] = updatedItem;
+    }
+
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    };
   }
   return defaultCartState;
 };
