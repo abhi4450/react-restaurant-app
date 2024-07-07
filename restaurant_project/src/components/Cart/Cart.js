a49ac3729dd93b9ebbc4296390d77cb3f365aa47
abhi4450 <abhishek.anshu1991>
2024-07-07 17:07:00 +0530

    added the logic to add items in the cart in reducer function

--- restaurant_project/src/components/Cart/Cart.js
+++ restaurant_project/src/components/Cart/Cart.js
@@ -1,31 +1,33 @@
-import React from "react";
+import React, { useContext } from "react";
 import Modal from "../UI/Modal";
 import classes from "./Cart.module.css";
+import CartContext from "../../store/cart-context";
+
 
 const Cart = (props) => {
-  
+  const cartCtx = useContext(CartContext);
   const cartItems = (
     <ul className={classes["cart-items"]}>
-      {[{ id: "c1", name: "Sushi", amount: 2, price: 12.99 }].map((item) => (
+      {cartCtx.items.map((item) => (
         <li>{item.name}</li>
       ))}
     </ul>
   );
-
-
-  return <Modal onClose={props.onClose} >
-    {cartItems}
-    <div className={classes.total}>
-      <span>Total Amount</span>
-      <span>35.62</span>
-    </div>
-    <div className={classes.actions}>
-      <button className={classes["button-alt"]} onClick={props.onClose}>
-        Close
-      </button>
-      <button className={classes.button}>Order</button>
-    </div>
-  </Modal>;
+  return (
+    <Modal onClose={props.onClose}>
+      {cartItems}
+      <div className={classes.total}>
+        <span>Total Amount</span>
+        <span>{cartCtx.totalAmount}</span>
+      </div>
+      <div className={classes.actions}>
+        <button className={classes["button-alt"]} onClick={props.onClose}>
+          Close
+        </button>
+        <button className={classes.button}>Order</button>
+      </div>
+    </Modal>
+  );
 };
 
 export default Cart;
